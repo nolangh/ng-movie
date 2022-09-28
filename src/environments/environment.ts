ef9745e3a5df75d176c81ffcaa3f57030aab2482
nolangh <nolanghiggins@gmail.com>
2022-09-28 01:12:03 -0600

    -a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'notflix-1de69',
+    appId: '1:876698212555:web:4b69b98a1feae60279634e',
+    storageBucket: 'notflix-1de69.appspot.com',
+    apiKey: 'AIzaSyBDG4SMqXp7ADk29oB-K__O3UiheFAe-c4',
+    authDomain: 'notflix-1de69.firebaseapp.com',
+    messagingSenderId: '876698212555',
+    measurementId: 'G-E8RFM9DXNQ',
+  },
+
+  production: false,
 };
 
 /*
